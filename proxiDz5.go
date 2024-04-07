3fe6eee4082e566d6c20313663fbc87666df75a1
ILDUS <ildus6696@gmail.com>
2024-04-07 21:40:18 +0300

    DZ5_2

--- proxiDz5.go
+++ proxiDz5.go
@@ -9,7 +9,6 @@ import (
 var number int = 0
 
 func main() {
-	// URL-ы ваших серверов обработки JSON запросов
 	server1URL, _ := url.Parse("http://localhost:8080")
 	server2URL, _ := url.Parse("http://localhost:8082")
 
@@ -25,11 +24,5 @@ func main() {
 		}
 	})
 
-	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//	proxy2 := httputil.NewSingleHostReverseProxy(server2URL)
-	//	proxy2.ServeHTTP(w, r)
-	//})
-
-	// Запуск HTTP-сервера на порту 8080
 	http.ListenAndServe(":9000", nil)
 }
